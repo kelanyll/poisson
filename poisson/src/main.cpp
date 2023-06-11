56e685b272c5be0ae91e880c95547fca7a3dd1a6
yllkelani <yll.kelani@hotmail.co.uk>
2023-06-11 18:07:48 +0000

    Train model using helper classes

--- poisson/src/main.cpp
+++ poisson/src/main.cpp
@@ -3,87 +3,96 @@
 #include "PoissonRegressionModel.hpp"
 #include <DataFrame/DataFrame.h>
 #include "PoissonRegressionTrainer.hpp"
-#include "goals-predict.hpp"
-
-using namespace std;
-using namespace BOOM;
-using namespace hmdf;
-
-struct Dataset {
-    std::vector<Ptr<PoissonRegressionData>> dataset;
-    int num_dims;
-
-    Dataset(std::vector<Ptr<PoissonRegressionData>> dataset_val, int num_dims_val) : dataset{dataset_val}, num_dims{num_dims_val} {};
-};
-
-struct MyData {
-    int y;
-    std::vector<double> x;
-
-    MyData(int y_val, std::vector<double> x_val) : y{y_val}, x{x_val} {}
-};
-
-
-Dataset get_dataset();
-void print_coefs(PoissonRegressionModel model);
+#include "data-types.hpp"
+#include "util.hpp"
 
 ULDataFrame get_data() {
+    // to be replaced with loading from csv
     ULDataFrame df;
     df.load_data(std::vector<unsigned long>{1,2,3}, 
         std::make_pair("home", std::vector<std::string>{"Wolves", "Sunderland","Chelsea"}),
         std::make_pair("away", std::vector<std::string>{"Sunderland", "Chelsea","Wolves"}),
-        std::make_pair("home_goals", std::vector<int>{0, 1, 2}),
-        std::make_pair("away_goals", std::vector<int>{2, 1, 0})
+        std::make_pair("home_goals", std::vector<unsigned int>{0, 2, 2}),
+        std::make_pair("away_goals", std::vector<unsigned int>{2, 1, 0})
+    );
+
+    return df;
+}
+
+/*
+home, away, home_goals, away_goals -> team, opponent, home, goals
+*/
+ULDataFrame transform_to_row_per_goals(ULDataFrame df) {
+    ULDataFrame new_df{};
+
+    std::vector<unsigned long> idx{df.get_index()};
+    new_df.load_index(idx.begin(), idx.end());
+
+    new_df.load_column<std::string>("team", df.get_column<std::string>("home"));
+    new_df.load_column<std::string>("opponent", df.get_column<std::string>("away"));
+    new_df.load_column<bool>("home", std::vector<bool>(get_num_rows(df), true));
+    new_df.load_column<unsigned int>("goals", df.get_column<unsigned int>("home_goals"));
+
+    ULDataFrame away_rows{};
+    
+    int num_rows = get_num_rows(df);
+    std::vector<unsigned long> away_rows_idx(num_rows);
+    std::iota(away_rows_idx.begin(), away_rows_idx.end(), idx.back() + 1);
+    away_rows.load_index(away_rows_idx.begin(), away_rows_idx.end());
+
+    away_rows.load_column<std::string>("team", df.get_column<std::string>("away"));
+    away_rows.load_column<std::string>("opponent", df.get_column<std::string>("home"));
+    away_rows.load_column<bool>("home", std::vector<bool>(get_num_rows(df), false));
+    away_rows.load_column<unsigned int>("goals", df.get_column<unsigned int>("away_goals"));
+
+    new_df.self_concat<ULDataFrame, std::string, bool, unsigned int>(away_rows);
+
+    return new_df;
+}
+
+ULDataFrame add_intercept(ULDataFrame df) {
+    df.load_column<unsigned int>("intercept", std::vector<unsigned int>(get_num_rows(df), 1));
+
+    return df;
+}
+
+void print_variables(std::vector<std::string> names, std::vector<double> coefs) {
+    std::copy(names.begin(), names.end(), std::ostream_iterator<std::string>(std::cout, " "));
+    std::cout << std::endl;
+    std::copy(coefs.begin(), coefs.end(), std::ostream_iterator<double>(std::cout, " "));
+    std::cout << std::endl;
+}
+
+ULDataFrame get_test_data() {
+    ULDataFrame df;
+    df.load_data(std::vector<unsigned long>{1,2,3}, 
+        std::make_pair("team", std::vector<std::string>{"Wolves", "Sunderland"}),
+        std::make_pair("opponent", std::vector<std::string>{"Sunderland", "Wolves"}),
+        std::make_pair("home", std::vector<bool>{true, false})
     );
 
     return df;
 }
 
 int main() {
-    ULDataFrame train_df{transform_to_row_per_goals(get_data())};
+    ULDataFrame train_df{add_intercept(transform_to_row_per_goals(get_data()))};
 
-    // const auto &col_ref = data_frame.get_column<std::string>("home");
-    // cout << col_ref[1] << col_ref[2] << endl;
-    // data_frame.write<std::ostream, std::string, std::string, int, int>(cout);
-    // std::pair shape = data_frame.shape();
-    // cout << "size: " << std::get<0>(shape) << "x" << std::get<1>(shape) << endl;
+    PoissonRegressionTrainer trainer{};
 
-    PoissonRegressionTrainer model = PoissonRegressionTrainer{std::move(train_df), "goals"};
+    PoissonRegressionModelData model_data{trainer.get_poisson_regression_model_data(std::move(train_df), "goals")};
+
+    PoissonRegressionModel model{model_data.x_col_names.size()};
+    model.set_data(model_data.data);
+    model.mle();
+    print_variables(model_data.x_col_names, model.coef().vectorize());
+
+    // transform_to_row_per_goals has to be able to convert test rows as well (without goals columns)
+    ULDataFrame test_df{add_intercept(get_test_data())};
     
-    ULDataFrame test_df{};
-    for (double y : model.predict(test_df)) {
-        cout << y << ",";
+    for (ConstVectorView x : trainer.generate_x(test_df, model_data)) {
+        double lambda = exp(model.predict(x));
+        std::cout << "Expected goals: " << lambda << std::endl;
     }
-    cout << endl;
 
     return 0;
-}
-
-/*
-home, away, home_goals, away_goals -> (ohe) team_.., opponent_.., goals, home
-function that reads the csv
-function to convert dataframe into expected format (reuse this to convert predicts into the same format)
-constructor on Dataset that takes a dataframe and converts it to PoissonRegression
-poissonregressionmodel doesn't care what the columns are called but we need it for ease of use
-- when we predict i want to be able to pass (team: wolves, opponent: sunderland, home: true)
-- then that converts it into arsenal_team: false, ..., wolves_team: true,..., home: true
-- so maybe we create an object that takes a dataframe and then trains the model based off it
-*/
-Dataset get_dataset() {
-    int num_dim = 2;
-    std::vector<MyData> data{MyData{10, std::vector<double>{1,2}}, MyData{7, std::vector<double>{1,3}}, MyData{5, std::vector<double>{1,4}}, MyData{3,std::vector<double>{1,5}}, MyData{2, std::vector<double>{1,6}}};
-
-    std::vector<Ptr<PoissonRegressionData>> ptrs{};
-    std::transform(data.begin(), data.end(), std::back_inserter(ptrs), [](MyData datum) -> Ptr<PoissonRegressionData> {
-        datum.x.push_back(1); // intercept
-        return Ptr<PoissonRegressionData>{new PoissonRegressionData{datum.y, Vector{datum.x}}};
-    });
-
-    return Dataset{ptrs, num_dim + 1};
-}
-
-void print_coefs(PoissonRegressionModel model) {
-    stringstream oss;
-    model.coef().vectorize().write(oss, true);
-    cout << "Parameters: " << oss.str();
 }
